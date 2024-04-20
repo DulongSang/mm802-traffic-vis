01b7ec744b080300e8ffa5b1b340669274f1b535
Dulong Sang <dulong@ualberta.ca>
2024-04-19 19:14:14 -0600

    fix: add flag to mock backend data

--- frontend/src/api/TrafficApi.ts
+++ frontend/src/api/TrafficApi.ts
@@ -1,26 +1,35 @@
 import axios from 'axios';
 
-import { BACKEND_BASE_URL } from "../config";
+import { BACKEND_BASE_URL, MOCK_BACKEND_DATA } from "../config";
 import { BarChartData } from '../components/dashboard/BarChartCard';
 import { LineChartData } from "../components/dashboard/LineChartCard";
 import { TrafficDisruption } from '../types/TrafficDisruption';
 
-export function getTrafficIndex(datetime: Date): Promise<number> {
-    const mockValue = Math.floor(Math.random() * 100);
-    return Promise.resolve(mockValue);
-    // return axios.get(`${BACKEND_BASE_URL}/traffic-index?datetime=${datetime.toISOString()}`);
+export async function getTrafficIndex(datetime: Date): Promise<number> {
+    if (MOCK_BACKEND_DATA) {
+        const mockValue = Math.floor(Math.random() * 100);
+        return Promise.resolve(mockValue);
+    }
+    const { data } = await axios.get(`${BACKEND_BASE_URL}/traffic-index?datetime=${datetime.toISOString()}`);
+    return data;
 }
 
-export function getTotalDisruption(datetime: Date): Promise<number> {
-    const mockValue = Math.floor(Math.random() * 100);
-    return Promise.resolve(mockValue);
-    // return axios.get(`${BACKEND_BASE_URL}/total-disruption?datetime=${datetime.toISOString()}`);
+export async function getTotalDisruption(datetime: Date): Promise<number> {
+    if (MOCK_BACKEND_DATA) {
+        const mockValue = Math.floor(Math.random() * 100);
+        return Promise.resolve(mockValue);
+    }
+    const { data } = await axios.get(`${BACKEND_BASE_URL}/total-disruption?datetime=${datetime.toISOString()}`);
+    return data;
 }
 
-export function getTrafficJams(datetime: Date): Promise<number> {
-    const mockValue = Math.floor(Math.random() * 100);
-    return Promise.resolve(mockValue);
-    // return axios.get(`${BACKEND_BASE_URL}/traffic-jams?datetime=${datetime.toISOString()}`);
+export async function getTrafficJams(datetime: Date): Promise<number> {
+    if (MOCK_BACKEND_DATA) {
+        const mockValue = Math.floor(Math.random() * 100);
+        return Promise.resolve(mockValue);
+    }
+    const { data } = await axios.get(`${BACKEND_BASE_URL}/traffic-jams?datetime=${datetime.toISOString()}`);
+    return data;
 }
 
 function generateMockTrend() {
@@ -35,26 +44,35 @@ function generateMockTrend() {
 }
 
 export async function getTrafficIndexTrend(period: string): Promise<LineChartData> {
-    const mockValue = generateMockTrend();
-    return mockValue;
-    // return axios.get(`${BACKEND_BASE_URL}/traffic-index-trend?period=${period}`);
+    if (MOCK_BACKEND_DATA) {
+        const mockValue = generateMockTrend();
+        return mockValue;
+    }
+    const { data } = await axios.get(`${BACKEND_BASE_URL}/traffic-index-trend?period=${period}`);
+    return data;
 }
 
 export async function getDisruptionImpactDistribution(): Promise<BarChartData> {
-    const mockData: BarChartData = {
-        series: [
-            { data: Array.from({ length: 5 }, () => Math.floor(Math.random() * 100)), label: 'Disruption' },
-        ],
-        xAxis: [{ data: ['Permanent Closure', 'Travel Delays', 'Directional Closure', 'Total Closure', 'Increased Travel Time'], scaleType: 'band' }],
+    if (MOCK_BACKEND_DATA) {
+        const mockData: BarChartData = {
+            series: [
+                { data: Array.from({ length: 5 }, () => Math.floor(Math.random() * 100)), label: 'Disruption' },
+            ],
+            xAxis: [{ data: ['Permanent Closure', 'Travel Delays', 'Directional Closure', 'Total Closure', 'Increased Travel Time'], scaleType: 'band' }],
+        }
+        return mockData;
     }
-    return mockData;
-    // return axios.get(`${BACKEND_BASE_URL}/disruption-impact-distribution`);
+    const { data } = await axios.get(`${BACKEND_BASE_URL}/disruption-impact-distribution`);
+    return data;
 }
 
-export function getTrafficJamsTrend(period: string): Promise<LineChartData> {
-    const mockValue = generateMockTrend();
-    return Promise.resolve(mockValue);
-    // return axios.get(`${BACKEND_BASE_URL}/traffic-jams-trend?period=${period}`);
+export async function getTrafficJamsTrend(period: string): Promise<LineChartData> {
+    if (MOCK_BACKEND_DATA) {
+        const mockValue = generateMockTrend();
+        return Promise.resolve(mockValue);
+    }
+    const { data } = await axios.get(`${BACKEND_BASE_URL}/traffic-jams-trend?period=${period}`);
+    return data;
 }
 
 function calculateProgress(startDate: string, finishDate: string): number {
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,2 @@
 export const BACKEND_BASE_URL = 'http://localhost:8000';
+export const MOCK_BACKEND_DATA = true;
